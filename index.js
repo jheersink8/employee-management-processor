03ae3fd790141a78f7471df32c275aaed64690ba
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-03-17 22:14:49 -0600

    All questions built out. Working on queries next.

--- index.js
+++ index.js
@@ -1,42 +1,60 @@
 // Import PostgreSQL Pool package, Inquirer, and inquirer questions (iq)
-const { Pool } = require('pg');
 const inquirer = require('inquirer');
 const iq = require('./scripts/questions');
+const query = require('./scripts/query')
 
-// Connect to database
-const pool = new Pool(
-    {
-        user: 'postgres',
-        password: '1234',
-        database: 'emp_db'
-    },
-)
-pool.connect();
+// Index variable used to determine which query to run 
+let index;
 
-// pool.query(`SELECT * FROM department`, function (err, {rows}) {
-//     console.log(rows);
-// })
+// User prompts through inquirer questions from "questions.js" file
+function init() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: iq.questions.name[0],
+            message: iq.questions.message[0],
+            choices: iq.questions.choices[0]
+        },
+    ])
+        // Dynamically choose next questions based on first input
+        .then((response) => {
+            for (var i = 0; i < iq.questions.choices[0].length; i++) {
+                if (response[iq.questions.name[0]] === iq.questions.choices[0][i]) {
+                    index = i + 1
+                    inquirer.prompt([
+                        {
+                            type: 'list',
+                            name: iq.questions.name[i + 1],
+                            message: iq.questions.message[i + 1],
+                            choices: iq.questions.choices[i + 1]
+                        }
+                    ])
+                        //Use the index code to determine which function and query to run 
+                        .then((response) => {
+                            questionIndex = response[iq.questions.name[index]]
+                            switch (iq.questions.name[index]) {
+                                case iq.questions.name[1]:
+                                    query.viewQuestions(questionIndex);
+                                    break;
 
-// User prompts through inquirer
-inquirer.prompt([
-    {
-        type: 'list',
-        name: iq.questions.name[0],
-        message: iq.questions.message[0],
-        choices: iq.questions.choices[0]
-    },
-])
-    .then((response) => {
-        for (var i = 0; i < iq.questions.choices[0].length; i++) {
-            if (response[iq.questions.name[0]] === iq.questions.choices[0][i]) {
-                inquirer.prompt([
-                    {
-                        type: 'list',
-                        name: iq.questions.name[i + 1],
-                        message: iq.questions.message[i + 1],
-                        choices: iq.questions.choices[i + 1]
-                    }
-                ])
+                                case iq.questions.name[2]:
+                                    query.addQuestions(questionIndex);
+                                    break;
+
+                                case iq.questions.name[3]:
+                                    query.updateQuestions(questionIndex);
+                                    break;
+
+                                case iq.questions.name[4]:
+                                    query.deleteQuestions(questionIndex);
+                                    break;
+                            }
+
+
+                        })
+                }
             }
-        }
-    });
+        });
+};
+
+init(); 
