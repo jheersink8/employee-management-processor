0bf9b316fcfd01b86a2ed8c9405ec63eb6ba59cb
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-03-19 20:48:09 -0600

    Successfully running 3 queries through entire production model.

---
+++ OLD TRASH/questions.js
@@ -0,0 +1,55 @@
+// Organized prompts for all possible inquirer questions in a three dimensional array: 
+const questions = {
+    // These are NOT dynamic variables. If you change them here, you have to update them elsewhere. 
+    name: ['initialQuestions', 'viewQuestions', 'addQuestions', 'updateQuestions', 'deleteQuestions'],
+    
+    message: ['ROOT MENU: I want to:', 'I want to view:', 'I want to add:', 'I want to update:', 'I want to delete:'],
+    
+    choices: [
+       
+        // Initial Questions //
+        [
+            'View reports from the EMP',
+            'Add data to the EMP',
+            'Update existing data in the EMP',
+            'Delete data from the EMP'
+        ],
+       
+        // "I want to view:" questions //
+        [
+            'all departments',
+            'all roles',
+            'all employees',
+            'all employees by manager',
+            'all employees by department',
+            'the total utalized employee budget of a department',
+        ],
+       
+        // "I want to add:" questions //
+        [
+            'a department to the organization',
+            'a role to a department',
+            'an employee to the organization',
+        ],
+
+        // "I want to update:" questions //
+        [
+            'an employee`s role',
+            'an employee`s manager',
+        ],
+
+        // "I want to delete:" questions// 
+        [
+            'a department',
+            'a role',
+            'an employee',
+        ]
+    ]
+};
+
+module.exports = { questions };
+
+
+
+
+
